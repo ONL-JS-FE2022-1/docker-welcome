10a7f2480a200440c20fab329ceb1de832fc0d6e
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-03-24 20:35:29 +0200

    Dockerfile project build

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use((req, res, next) => {
+    res.status(200).send('HIIIII AGAIN')
+})
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`$App statrted on port ${PORT}`)
+})
